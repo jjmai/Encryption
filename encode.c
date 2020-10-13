04abb38ad8d9a5a3a39ff3c653ce9467b2a6c147
Jordan Mai <jmai12@unix4.lt.ucsc.edu>
2020-10-12 23:51:20 -0700

    reading and trie should work

--- encode.c
+++ encode.c
@@ -1,13 +1,14 @@
+#include "code.h"
+#include "io.h"
+#include "trie.h"
+#include <fcntl.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include "trie.h"
-#include "io.h"
-#include <fcntl.h> 
-#include<sys/types.h>
-#include<sys/stat.h> 
 int main(int argc, char *argv[]) {
   int infile = STDIN_FILENO;
   int outfile = STDOUT_FILENO;
@@ -21,17 +22,38 @@ int main(int argc, char *argv[]) {
       break;
     case 'o':
       outfile = open(optarg, O_WRONLY | O_CREAT | O_TRUNC);
-       break;
+      break;
     default:
       printf("?");
       exit(1);
     }
   }
   // read in
-  uint8_t curr_sym=0;
-  
-  while(read_sym(infile,&curr_sym)); 
   TrieNode *root = trie_create();
-  
+  TrieNode *curr_node = root;
+  TrieNode *prev_node = NULL;
+  uint8_t curr_sym = 0;
+  uint8_t prev_sym = 0;
+  uint16_t next_code = START_CODE;
+
+  while (read_sym(infile, &curr_sym)) {
+    TrieNode *next_node = trie_step(curr_node, curr_sym);
+    if (next_node != NULL) {
+      prev_node = curr_node;
+      curr_node = next_node;
+       //printf("%c\n",curr_sym);
+    } else {
+      buffer_pair(outfile, curr_node->code, curr_sym, log2(next_code) + 1);
+      curr_node->children[curr_sym] = trie_node_create(next_code);
+      // printf("%c %d\n",curr_sym,curr_node->children[curr_sym]->code);
+      curr_node = root;
+      next_code += 1;
+    }
+
+    prev_sym = curr_sym;
+  }
+
+  // buffer_pair(outfile,3,&curr_sym,16);
+
   close(infile);
 }
--- io.c
+++ io.c
@@ -1,24 +1,41 @@
 #include "io.h"
 #include "code.h"
-#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <fcntl.h>
 #include <unistd.h>
 #define BLOCK 4096
 static uint8_t buffer[BLOCK];
-static int lines=0,line_count=0;
+static uint8_t word[BLOCK];
+static int lines = 0, line_count = 0, bit_index;
 
-//read each character and return
+// read each character and return
 bool read_sym(int infile, uint8_t *sym) {
-  if(line_count==0) {
-    lines = read(infile,buffer, BLOCK);
-    if(lines==0) return false;
-   }
-   *sym = buffer[line_count];
-   printf("%c\n",*sym);
-   line_count=(line_count+1)%lines;
+  if (line_count == 0) {
+    lines = read(infile, buffer, BLOCK);
+    if (lines == 0)
+      return false;
+  }
+  *sym = buffer[line_count];
+  line_count = (line_count + 1) % lines;
+
+  return true;
+}
+
+void buffer_pair(int outfile, uint16_t code, uint8_t sym, uint8_t bitlen) {
+  uint16_t temp = 0;
+  //printf("%d code, %c sym, %d bitlen ",code,sym,bitlen);
+  for (int i = 0; i < bitlen; i++) {
+    if (code & (1 << i)) {
+      word[bit_index / 8] |= 1 << (bit_index % 8);
+     // printf("1");
+    } else {
+      word[bit_index / 8] &= ~(1 << (bit_index % 8));
+      //printf("0");
+    }
+  }
   
-  return true; 
+ // printf("\n");
 }
